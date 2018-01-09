5bb4aa26a76ff32fa44f3df5b6f7687b08023e25
H. M. Mahedi Hasan <jisan2723@gmail.com>
2018-01-09 15:42:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from flask import make_response
 
 app = Flask(__name__)
 
-from googleapiclient.discovery import build
-
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -21,34 +19,6 @@ def webhook():
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
-
-
-def google_search(search_term, api_key, cse_id, **kwargs):
-    service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
-
-
-def makeWebhookResult(data, searchstring):
-    if (data[0] is None):
-        return {}
-
-    articleUrl = data[0].get('formattedUrl')
-    # print(json.dumps(item, indent=4))
-
-    speech = "Please view this article for more information on " + searchstring + ": " \
-             + articleUrl
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "google-search-webhook"
-    }
 
 
 def MakeWebRequest(req):
@@ -65,26 +35,6 @@ def MakeWebRequest(req):
             "displayText": speech,
             "source": "Athena"
         }
-    elif req.get("result").get("action") != "googleSearch":
-        json_params = req.get("result").get("parameters")
-        searchstring = ''  # this creates the overall topic which covers user's raw query
-
-        for value in json_params.values():
-            searchstring += value
-            searchstring += " "
-        print(searchstring)
-        searchString = "robot %s" % searchstring
-
-        # KEYS SHOULDNT BE DISPLAYED
-        my_api_key = ""
-        my_cse_id = ""
-        searchResults = google_search(searchString, my_api_key, my_cse_id, num=1)  # search for the topic
-
-        if searchResults is None:
-            return {}
-
-        res = makeWebhookResult(searchResults, searchstring)
-        return res
     elif req.get("result").get("action") == "Maths":
         result = req.get("result")
         parameters = result.get("parameters")
